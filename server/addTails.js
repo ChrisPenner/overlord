34735aea5e8aacff0d158dc5c505f9042401e0f1
Chris Penner <christopher.penner@gmail.com>
2016-04-14 15:37:18 -0600

    Complete refactoring to use message-passing.

--- server/addTails.js
+++ server/addTails.js
@@ -14,14 +14,6 @@ function addTails(filenames, callback){
     }
 }
 
-function getFileNames(logDir, callback){
-    fs.readdir(logDir, function(err,files){
-        if(err){ throw err; }
-        const filenames = files.map((filename) => path.resolve(logDir, filename))
-        callback(filenames);
-    });
-}
-
 function getLogsFromFiles(files, callback){
     for (const filename of files){
         fs.readFile(filename, "utf-8", function (err, text) {
@@ -32,18 +24,22 @@ function getLogsFromFiles(files, callback){
     }
 }
 
-function getLocations(locations, callback){
+function getFileNames(locations, callback){
     for (let location of locations){
-        console.log('location:', location)
         fs.stat(location, (err, locationStats) => {
             if (err) throw err;
+
             if (locationStats.isDirectory()) {
-                getFileNames(location, callback);
+                fs.readdir(location, function(err,files){
+                    if(err){ throw err; }
+                    const filenames = files.map((filename) => path.resolve(location, filename))
+                    callback(filenames);
+                });
             } else {
-                callback(filenames);
+                callback([location]);
             }
         });
     }
 }
 
-export { addTails, getLocations, getFileNames, getLogsFromFiles}
+export { addTails, getFileNames, getLogsFromFiles}
--- server/main.js
+++ server/main.js
@@ -1,10 +1,13 @@
-import {addTails, getLocations, getLogsFromFiles} from './addTails'
+import {addTails, getFileNames, getLogsFromFiles} from './addTails'
 import electron from 'electron'
 import storage from 'electron-json-storage'
 import _ from 'lodash'
+import addLifeCycleHooks from './life-cycle'
 
 // Module to control application life.
 const app = electron.app;
+addLifeCycleHooks(app);
+
 const dialog = electron.dialog;
 const ipc = electron.ipcMain;
 // Module to create native browser window.
@@ -14,81 +17,63 @@ const BrowserWindow = electron.BrowserWindow;
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+let userData = {logFiles: []};
+
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({width: 1600, height: 1000, center: true});
-
-  // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/../app/html/index.html');
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools();
-
-  const init = (filename, lines) => mainWindow.webContents.send('init', filename, lines);
-  const trackLogs = (filename, line) => mainWindow.webContents.send('logs', filename, line);
-  // storage.set('userData', {})
-
-  ipc.on('openLogLocationDialog', () => {
-      dialog.showOpenDialog( {
-          title: 'Choose log files or directories',
-          defaultPath: '~/',
-          properties: [ 'openFile', 'openDirectory', 'multiSelections' ]
-      }, (locations) => {
-          storage.get('userData', (err, data) => {
-              console.log('log-files:', data);
-              const existingLogFiles = data.logFiles || [];
-              data.logFiles = _.union(existingLogFiles, locations);
-              console.log('union', data.logFiles);
-              storage.set('userData', data);
-              const newLocations = _.difference(locations, existingLogFiles);
-              console.log('newLocations', newLocations);
-              initNewFiles(newLocations);
-          });
-      });
+  mainWindow.webContents.on('did-finish-load', () => { 
+      ipc.emit('newWindow', mainWindow); 
   });
 
-  function initNewFiles(paths){
-      getLocations(paths, (filenames) => {
-          getLogsFromFiles(filenames, init);
-          addTails(filenames, trackLogs);
-      });
-  }
-
-  mainWindow.webContents.on('did-finish-load', function() {
-      storage.get('userData', (error, data) =>{
-          if (error) throw error;
-
-          const locations = data.logFiles || [];
-          initNewFiles(locations);
-      });
-  });
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
+  mainWindow.on('closed', () => {
+      mainWindow = null;
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+ipc.on('openLogLocationDialog', () => {
+    dialog.showOpenDialog( {
+        title: 'Choose log files or directories',
+        defaultPath: '~/',
+        properties: [ 'openFile', 'openDirectory', 'multiSelections' ]
+    }, (locations) => {
+        const existingLogFiles = userData.logFiles || [];
+        userData.logFiles = _.union(existingLogFiles, locations);
+        storage.set('userData', userData);
+        const newLocations = _.difference(locations, existingLogFiles);
+        ipc.emit('newLocations', newLocations);
+    });
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+ipc.on('newWindow', (window) => {
+    const paths = userData.logFiles;
+    getFileNames(paths, (filenames) => {
+        getLogsFromFiles(filenames, (filename, lines) => {
+            window.webContents.send('init', filename, lines)
+        });
+    });
+});
+
+ipc.on('newLocations', (paths) => {
+    getFileNames(paths, (filenames) => {
+        getLogsFromFiles(filenames, (filename, lines) => {
+            if (mainWindow) {
+                mainWindow.webContents.send('init', filename, lines);
+            }
+        });
+        addTails(filenames, (filename, line) => {
+            if (mainWindow) {
+                mainWindow.webContents.send('logs', filename, line)
+            }
+        });
+    });
+});
+
+storage.get('userData', (error, data) => {
+    if (error) throw error;
+    userData = _.defaults(data, {logFiles: []});
+    ipc.emit('newLocations', userData.logFiles);
 });
